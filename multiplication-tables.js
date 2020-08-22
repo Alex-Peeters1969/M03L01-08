1be99f8423b17a693a94eec43a03c7d7002ec162
Alex-Peeters1969 <67190405+Alex-Peeters1969@users.noreply.github.com>
2020-08-22 13:11:28 +0200

    Add files via upload

---
+++ multiplication-tables.js
@@ -0,0 +1,14 @@
+
+for (let multi = 9; multi <= 9; multi++) {
+    for (let i = 1; i <= 10; i++) {
+      count = i * multi;
+      console.log(i + " X " + multi + " = " + count);
+    }
+  };
+
+  for (let multi = 1; multi <= 10; multi++) {
+    for (let i = 1; i <= 10; i++) {
+      count = i * multi;
+      console.log(i + " X " + multi + " = " + count);
+    }
+  };
